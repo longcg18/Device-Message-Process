8e4f25cdd8d8e7587a81697a64f9f00e56f8a611
Phuc Long Nguyen <90314029+longcg18@users.noreply.github.com>
2023-07-07 17:24:25 +0700

    Refactor folder and file locations

---
+++ app.py
@@ -0,0 +1,19 @@
+import sys
+
+log_files = []
+
+template_files = []
+
+title = []
+
+def export_results():
+    print()
+
+def export_title():
+    print()
+
+def export_results():
+    print()
+
+def get_device_info():
+    print()
