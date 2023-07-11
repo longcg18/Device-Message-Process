2e915b51a428e1dabf19832dc313c32613fabbb2
Phuc Long Nguyen <90314029+longcg18@users.noreply.github.com>
2023-07-11 22:44:33 +0700

    Update readme and Example

--- An_Example_Device/process.py
+++ An_Example_Device/process.py
@@ -1,55 +1,59 @@
 import textfsm as tf
 import openpyxl
 
+# Import module ReadLogFile
 import os, sys
 script_dir = os.path.dirname( __file__ )
 mymodule_dir = os.path.join( script_dir, '..')
 sys.path.append( mymodule_dir )
 import ReadLogFile
 
-if (__name__ == '__main__'):
-    fileName = "NAN0184ASW01_10.239.119.26_S3100-28FC.txt"
+# Main 
 
+if (__name__ == '__main__'):
+    
+    # Get device information
+    fileName = "deviceName_deviceIP_deviceModel.txt"
     deviceInfos = fileName.split('_')
     deviceName = deviceInfos[0]
     deviceIP = deviceInfos[1]
     deviceModel = deviceInfos[2]
 
-    with open(deviceName + ".template") as tpl:
-        fsm = tf.TextFSM(tpl)
+    # Read Template
+    with open("example.template") as template:
+        fsm = tf.TextFSM(template)
 
-    start_marker = "Port    Desc   Link shutdn Speed         Pri PVID Mode TagVlan    UtVlan"
-    end_marker = "Total entries: 28 ."
+    # Read data from log
+    start_marker = "Interface       Status              Description"
+    end_marker = "@@BLOCK--"
     data = ReadLogFile.read_data(fileName, start_marker, end_marker)
 
+    # Parse to results
     results = fsm.ParseText(data)
 
-    workbook = openpyxl.load_workbook("..\DataCollection.xlsx")
+    # Write results to WorkSheet (xlsx file)
+    workbook = openpyxl.load_workbook("..\Your_work_book.xlsx")
+
+    ## Create new sheet
     sheetName = str(deviceName)
     if (sheetName in workbook.sheetnames) == True:
         workbook.remove(workbook[sheetName]) 
     worksheet = workbook.create_sheet(sheetName)
-    title = ["Port", "Description", "LinkState", "ShutDownStatus", "Operate", "Speed", "Pri", "Mode", "TagVlan", "UtVlan"]
+
+    ## Set title for columns
+    title = ["Interface", "Status", "Description"]
     for col, val in enumerate(title, start=1):
         worksheet.cell(row=1, column=col).value = val
     row_num=2
     for result in results:
-        port = result[0]
-        desc = result[1]
-        link_state = result[2]
-        shutdn = result[3]
-        speed = result[4]
-        pri = result[5]
-        pvid = result[6]
-        mode = result[7]
-        tag_vlan = result[8]
-        ut_vlan = result[9]
-
-        line = [port, desc, link_state, shutdn, speed, pri, pvid, mode, tag_vlan, ut_vlan]
+        interface = result[0]
+        status = result[1]
+        description = result[2]
+        line = [interface, status, description]
         for col_num, res in enumerate(line, start=1):
             worksheet.cell(row_num, col_num).value=res
         row_num = row_num + 1
         
-    workbook.save("..\DataCollection.xlsx")
+    ## Save data and close
+    workbook.save("..\Your_work_book.xlsx")
     workbook.close()
-    
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Log_get_port_nims1
+# Log get port 
  Get port information
+
+## Decribe: device log -> excel file
+
+- One device log (text file) will be put in one folder, together with <b>one or more</b> **template** files and one **process.py** 
+
+log ----> template(s) ----> process.py ----> excel file 
+
+**NOTE**: View An_Example_Device
